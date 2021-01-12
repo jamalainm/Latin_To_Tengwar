d81c538e186a64632023c34214d4032aeba203fe
jamalainm <robot@inman.xyz>
2021-01-12 11:27:20 -0600

    Added inverting of silmë and essë if followed by a vowel, as well as some other conventions, like word-initial /h/ and /nw/

--- latin_to_tengwar.py
+++ latin_to_tengwar.py
@@ -3,11 +3,17 @@
 This file converts the latin alphabet to the character encoding
 used by the tengwar annatar font. It will not display tengwar
 without the font being installed, too.
+
+example usage:
+    >>> from latin_to_tengwar import transcribe_sentence
+    >>> transcribe_sentence("quetin i lambë eldaiva")
 """
 
 import re
 
 vowels = ['a','e','i','o','u','á','é','í','ó','ú','ä','ë','ï','ö','ü']
+
+tengwar_vowels = ['C','F','G','H','J']
 
 combined_vowels = {
         'a' : 'C',
@@ -71,12 +77,15 @@ doubles = {
         'ty' : '1Ì',
         'sy' : 'iÌ',
         'ny' : '5Ì',
-        'ry' : '7Ì',
+        'ry' : '6Ì',
         'ly' : 'jÌ',
         'tt' : "1'",
         'nn' : "5'",
         'll' : "j'",
         'mm' : "t'",
+        'ss' : "k",
+        'hr' : "Á6",
+        'hl' : "Áj",
         }
 
 triples = {
@@ -84,6 +93,27 @@ triples = {
         'nqu': 'v',
         'ndy': '2Ì',
         'nty': '4Ì',
+        'ssa': ",C",
+        'sse': ",F",
+        'ssi': ",G",
+        'sso': ",H",
+        'ssu': ",J",
+        'ssë': ",F",
+        'sai': '8lC',
+        'sau': '8.C',
+        'soi': '8lH',
+        'sui': '8lJ',
+        'seu': '8.F',
+        'siu': '8.G',
+        }
+
+quads = {
+        'ssai' : 'klC',
+        'ssau' : 'k.C',
+        'ssoi' : 'klH',
+        'ssui' : 'klJ',
+        'sseu' : 'k.F',
+        'ssiu' : 'k.G',
         }
 
 consonants = {
@@ -101,11 +131,15 @@ consonants = {
         'w' : 'n',
         'l' : 'j',
         'h' : 'd',
-        's' : 'i',
+        's' : '8',
+        'þ' : '3',
+        'x' : 'aÇ',
         }
 
 initial_consonant = {
-        'h' : '9'
+        'h' : '9',
+        'y' : 'hÌ',
+        'r' : '7',
         }
 
 class LatinText:
@@ -155,6 +189,37 @@ class LatinWord:
     def transliterate(self):
         first = True
         word_length = len(self.latin_word)
+
+        while word_length > 3:
+            """ run through all substitutions """
+            if self.quad_check():
+                self.quad_substitute()
+                word_length -= 4
+            if self.triple_check():
+                self.triple_substitute()
+                word_length -= 3
+            elif self.double_check():
+                self.double_substitute()
+                word_length -= 2
+            elif self.initial_vowel_check():
+                self.independent_vowel_substitute()
+                word_length -= 1
+            elif self.initial_h_check():
+                self.initial_h_substitute()
+                word_length -= 1
+            elif self.long_vowel_check():
+                self.long_vowel_substitute()
+                word_length -= 1
+            elif self.preceding_vowel_check():
+                self.independent_vowel_substitute()
+                word_length -= 1
+            elif self.combined_vowel_check():
+                self.combined_vowel_substitute()
+                word_length -= 1
+            elif self.consonant_check():
+                self.consonant_substitute()
+                word_length -= 1
+
         while word_length > 2:
             """ run through all substitutions """
             if self.triple_check():
@@ -228,6 +293,15 @@ class LatinWord:
                 word_length -= 1
 
 
+    def quad_check(self):
+        if len(self.latin_word) == 4:
+            if self.latin_word in quads.keys():
+                return True
+        elif self.latin_word[:4] in quads.keys():
+            return True
+        else:
+            return False
+            
     def triple_check(self):
         if len(self.latin_word) == 3:
             if self.latin_word in triples.keys():
@@ -242,13 +316,26 @@ class LatinWord:
             if self.latin_word in doubles.keys():
                 return True
         elif self.latin_word[:2] in doubles.keys():
+            if self.latin_word[:2] == 'nw':
+                if len(self.annatar_word) == 0:
+                    return True
+                else:
+                    return False
             return True
         else:
             return False
 
+    def quad_substitute(self):
+        if len(self.latin_word) == 4:
+            self.annatar_word += self.latin_word
+            self.latin_word = ""
+        else:
+            self.annatar_word += quads[self.latin_word[:4]]
+            self.latin_word = self.latin_word[4:]
+
     def triple_substitute(self):
         if len(self.latin_word) == 3:
-            self.annatar_word += self.latin_word
+            self.annatar_word += triples[self.latin_word]
             self.latin_word = ""
         else:
             self.annatar_word += triples[self.latin_word[:3]]
@@ -267,7 +354,7 @@ class LatinWord:
             return True
 
     def initial_h_check(self):
-        if self.latin_word[0] == 'h' and len(self.annatar_word) == 0:
+        if self.latin_word[0] in initial_consonant.keys() and len(self.annatar_word) == 0:
             return True
 
     def independent_vowel_substitute(self):
@@ -278,7 +365,7 @@ class LatinWord:
             self.latin_word = self.latin_word[1:]
 
     def initial_h_substitute(self):
-        self.annatar_word += '9'
+        self.annatar_word += initial_consonant[self.latin_word[0]]
         if len(self.latin_word) == 1:
             self.latin_word = ""
         else:
@@ -308,11 +395,23 @@ class LatinWord:
             return True
 
     def consonant_substitute(self):
-        self.annatar_word += consonants[self.latin_word[0]]
-        if len(self.latin_word) == 1:
-            self.latin_word = ""
+        if self.latin_word[0] == 'r':
+            if len(self.latin_word) > 1:
+                if self.annatar_word[-1] in tengwar_vowels and self.latin_word[1] in vowels:
+                    self.annatar_word += '7'
+                    self.latin_word = self.latin_word[1:]
+                else:
+                    self.annatar_word += '6'
+                    self.latin_word = self.latin_word[1:]
+            else:
+                self.annatar_word += '6'
+                self.latin_word = ''
         else:
-            self.latin_word = self.latin_word[1:]
+            self.annatar_word += consonants[self.latin_word[0]]
+            if len(self.latin_word) == 1:
+                self.latin_word = ""
+            else:
+                self.latin_word = self.latin_word[1:]
 
     def long_vowel_check(self):
         if self.latin_word[0] in long_vowels.keys():
@@ -343,5 +442,5 @@ def transcribe_sentence(sentence):
 
 
 if __name__ == "__main__":
-    my_sentence = transcribe_sentence("Hísiel cápa nómerya foinallo")
+    my_sentence = transcribe_sentence("hísiel cápa nómerya foinallo")
     print(my_sentence)
